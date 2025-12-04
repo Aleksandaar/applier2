12b423593326d5bb0d9be52f99f29be8c946da5a
Aleksandar <aleksandar@zoric.me>
2025-12-04 16:05:16 +0100

    Add default email for emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   include Rails.application.routes.url_helpers
   
-  default from: App::Config.mailer.default_from
+  default from: App::Config.mailer.no_replay_email
   layout "mailer"
 
   default_url_options = { host: App::Config.mailer.default_site_host }
